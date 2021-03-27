d45d00ba492cf1ec7a7e7e295da1283b128af1d3
Keji Oyekunle <teleayo.o@gmail.com>
2021-03-27 01:10:19 +0100

    Add course list table

--- backend/webapp/models/course.py
+++ backend/webapp/models/course.py
@@ -1,6 +1,10 @@
 from webapp.extensions import db 
 
 
+course_list = db.Table('course_list',
+    db.Column('course_id', db.String(7), db.ForeignKey('course.code'), primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+)
 
 class Course(db.Model): 
     __tablename__ = 'course'
@@ -8,4 +12,11 @@ class Course(db.Model):
     code = db.Column(db.String(7), primary_key=True)
     title = db.Column(db.String(80), nullable=False)
     credits = db.Column(db.Integer, nullable=False)
+
+    # many -to many with Users
+    users = db.relationship('User', secondary=course_list, lazy='subquery',
+        backref=db.backref('courses', lazy=True))
     # study_blocks = db.relationship('study_block', backref='course', lazy=True)
+
+
+
